7db97c3b9fcaf042253860f1239f925c0efc1525
FoxnArt <111170011+FoxnArt@users.noreply.github.com>
2023-04-11 09:13:53 +0300

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -10,30 +10,32 @@ public class Server {
 
     public static void main(String[] args) throws IOException {
         System.out.println("Запускаем сервер на порту: " + PORT);
-        ServerSocket serverSocket = new ServerSocket(PORT);
 
-        Socket clientSocket = serverSocket.accept();
-
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-        System.out.println("- " + in.readLine());
-        out.println("Server: Write your name?"); System.out.println("Server: Write your name?");
-        String username = in.readLine(); System.out.println("- " + username);
-        out.println("Server: Are you child? (yes/no)"); System.out.println("Server: Are you child? (yes/no)");
-        String answer = in.readLine(); System.out.println("- " + answer);
-        if (answer.equals("Yes")) {
-            out.printf("Server: Welcome to the kids area, %s! Let's play!", username);
-            System.out.printf("Server: Welcome to the kids area, %s! Let's play!", username);
-        } else if (answer.equals("No")) {
-            out.printf("Server: Welcome to the adult zone, %s! Have a good rest, or a good working day!", username);
-            System.out.printf("Server: Welcome to the adult zone, %s! Have a good rest, or a good working day!", username);
-        } else {
-            out.printf("Server: %s, answer is not correct", username);
-            System.out.printf("Server: %s, answer is not correct", username);
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            try (Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+            ) {
+                System.out.println("- " + in.readLine());
+                out.println("Server: Write your name?");
+                System.out.println("Server: Write your name?");
+                String username = in.readLine();
+                System.out.println("- " + username);
+                out.println("Server: Are you child? (yes/no)");
+                System.out.println("Server: Are you child? (yes/no)");
+                String answer = in.readLine();
+                System.out.println("- " + answer);
+                if (answer.equals("Yes")) {
+                    out.printf("Server: Welcome to the kids area, %s! Let's play!", username);
+                    System.out.printf("Server: Welcome to the kids area, %s! Let's play!", username);
+                } else if (answer.equals("No")) {
+                    out.printf("Server: Welcome to the adult zone, %s! Have a good rest, or a good working day!", username);
+                    System.out.printf("Server: Welcome to the adult zone, %s! Have a good rest, or a good working day!", username);
+                } else {
+                    out.printf("Server: %s, answer is not correct", username);
+                    System.out.printf("Server: %s, answer is not correct", username);
+                }
+            }
         }
-
-        clientSocket.close();
-        serverSocket.close();
     }
 }
